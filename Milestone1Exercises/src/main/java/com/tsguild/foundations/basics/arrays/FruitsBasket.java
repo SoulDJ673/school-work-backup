2b88cc6135393c8b6924ec972d97d01c299307c2
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-08-15 22:48:11 -0400

    Completed some more Arrays

---
+++ Milestone1Exercises/src/main/java/com/tsguild/foundations/basics/arrays/FruitsBasket.java
@@ -0,0 +1,22 @@
+package com.tsguild.foundations.basics.arrays;
+
+/**
+ *
+ * @author souldj673
+ */
+public class FruitsBasket {
+
+    public static void main(String[] args) {
+        String[] fruit = {"Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Orange"};
+        String[] apples;
+        String[] oranges;
+
+        System.out.println("Total # of fruit in basket: " + fruit.length);
+
+        //Fruit Sorting
+        for (int i = 0; i < fruit.length; i++) {
+
+        }
+    }
+
+}
--- Milestone1Exercises/src/main/java/com/tsguild/foundations/basics/arrays/HiddenNuts.java
+++ Milestone1Exercises/src/main/java/com/tsguild/foundations/basics/arrays/HiddenNuts.java
@@ -1,21 +1,27 @@
 package com.tsguild.foundations.basics.arrays;
 
 import java.util.Random;
+
 /**
  *
  * @author souldj673
  */
 public class HiddenNuts {
+
     public static void main(String[] args) {
         //Setup hiding array & random
         String[] hidingSpots = new String[100];
         Random squirrel = new Random();
-        
+
         //Hiding nut
         hidingSpots[squirrel.nextInt(hidingSpots.length)] = "Nut";
-        System.out.println("The nut (gachiHYPER) has been hidden...");
-        
+        System.out.println("The nut has been hidden...");
+
         //Finding Nut
-        
+        for (int i = 0; i < hidingSpots.length; i++) {
+            if ("Nut".equals(hidingSpots[i])) {
+                System.out.println("Found it! It's in spot " + i);
+            }
+        }
     }
 }
