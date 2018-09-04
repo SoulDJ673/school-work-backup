ded54eb185bc683d28aa3e5b2a8b5751313b60b3
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-09-04 01:29:17 -0400

    Kraken Exercise - While Loops

--- Milestone1Exercises/src/main/java/com/tsguild/foundations/flowcontrol/whiles/BewareTheKraken.java
+++ Milestone1Exercises/src/main/java/com/tsguild/foundations/flowcontrol/whiles/BewareTheKraken.java
@@ -1,5 +1,7 @@
 package com.tsguild.foundations.flowcontrol.whiles;
 
+import java.util.Scanner;
+
 /**
  *
  * @author souldj673
@@ -7,6 +9,9 @@ package com.tsguild.foundations.flowcontrol.whiles;
 public class BewareTheKraken {
 
     public static void main(String[] args) {
+        //Scanner & Response Setup
+        Scanner userAnswer = new Scanner(System.in);
+        String userResponse;
 
         System.out.println("Already, get yer flippers & wetsuit on - we're going diving!");
         System.out.println("Here we goooOOooOooo.....! *SPLOOSH*");
@@ -18,6 +23,26 @@ public class BewareTheKraken {
         while (depthDivedInFt < 36200) {
             System.out.println("So far, we've swam " + depthDivedInFt + " feet");
 
+            //User Continue Prompt
+            System.out.print("Would you like to continue? [y/n] ");
+            userResponse = userAnswer.next();
+            try {                                                                                   //Input Validation
+
+                // Yes Continues Swim
+                if (userResponse.equalsIgnoreCase("y") || userResponse.equalsIgnoreCase("yes")) {
+                    System.out.println("\nOkay! Let's go!");
+
+                    //No Ends Voyage
+                } else if (userResponse.equalsIgnoreCase("n") || userResponse.equalsIgnoreCase("no")) {
+                    System.out.println("\nOkay. Time to go.");
+                    break;
+                }
+
+            } catch (Exception e) {
+                System.out.println("\nIt's a yes or no question.  Your stupidity causes you to continue swimming.");
+            }
+            
+            //Escape Kraken at below 20000
             if (depthDivedInFt >= 20000) {
                 System.out.println("OwO I think I see a Kraken, guys ....");
                 System.out.println("TIME TO GO!");
