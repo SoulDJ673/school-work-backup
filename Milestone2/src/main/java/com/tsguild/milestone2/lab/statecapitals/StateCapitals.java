811d3cd4db70a00204338f5825c027e1d5db88ef
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-09-06 10:14:18 -0400

    Finished and Fixed State Capitals 1

--- Milestone2/src/main/java/com/tsguild/milestone2/lab/statecapitals/StateCapitals.java
+++ Milestone2/src/main/java/com/tsguild/milestone2/lab/statecapitals/StateCapitals.java
@@ -1,6 +1,8 @@
 package com.tsguild.milestone2.lab.statecapitals;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -15,68 +17,93 @@ public class StateCapitals {
     public static void main(String[] args) {
         //Calls Map Building Method
         mapBuild();
-        
+
+        //Put Map values into collections for proper printouts
+        Collection<String> states = stateCapitalPairs.keySet();
+        Collection<String> capitals = stateCapitalPairs.values();
+
         //Print States to Console
         System.out.println("STATES");
-        System.out.println(stateCapitalPairs.keySet());
-        
+        infoPrinter(states);
+
+        System.out.println("");
+
         //Print Capitals to Console
         System.out.println("CAPITALS");
-        System.out.println(stateCapitalPairs.values());
+        infoPrinter(capitals);
+
+        System.out.println("");
+
+        //Print State/Capital Pair
+        System.out.println("STATES / CAPITALS");
+        infoPrinter(states, stateCapitalPairs);
 
     }
 
     private static void mapBuild() {
         //Add All States and Capitals to Map
-        stateCapitalPairs.put("\n\nAlabama", "\nMontgomery");
-        stateCapitalPairs.put("\n\nAlaska", "\nJuneau");
-        stateCapitalPairs.put("\nArizona", "\nPhoenix");
-        stateCapitalPairs.put("\nArkansas", "\nLittle Rock");
-        stateCapitalPairs.put("\nCalifornia", "\nSacramento");
-        stateCapitalPairs.put("\nColorado", "\nDenver");
-        stateCapitalPairs.put("\nConnecticut", "\nHartford");
-        stateCapitalPairs.put("\nDelaware", "\nDover");
-        stateCapitalPairs.put("\nFlorida", "\nTallahassee");
-        stateCapitalPairs.put("\nGeorgia", "\nAtlanta");
-        stateCapitalPairs.put("\nHawaii", "\nHonolulu");
-        stateCapitalPairs.put("\nIdaho", "\nBoise");
-        stateCapitalPairs.put("\nIllinois", "\nSpringfield");
-        stateCapitalPairs.put("\nIndiana", "\nIndianapolis");
-        stateCapitalPairs.put("\nIowa", "\nDes Moines");
-        stateCapitalPairs.put("\nKansas", "\nTopeka");
-        stateCapitalPairs.put("\nKentucky", "\nFrankfort");
-        stateCapitalPairs.put("\nLouisiana", "\nBaton Rouge");
-        stateCapitalPairs.put("\nMaine", "\nAugusta");
-        stateCapitalPairs.put("\nMaryland", "\nAnnapolis");
-        stateCapitalPairs.put("\nMassachusetts", "\nBoston");
-        stateCapitalPairs.put("\nMichigan", "\nLansing");
-        stateCapitalPairs.put("\nMinnesota", "\nSt. Paul");
-        stateCapitalPairs.put("\nMississippi", "\nJackson");
-        stateCapitalPairs.put("\nMissouri", "\nJefferson City");
-        stateCapitalPairs.put("\nMontana", "\nHelena");
-        stateCapitalPairs.put("\nNebraska", "\nLincoln");
-        stateCapitalPairs.put("\nNevada", "\nCarson City");
-        stateCapitalPairs.put("\nNew Hampshire", "\nConcord");
-        stateCapitalPairs.put("\nNew Jersey", "\nTrenton");
-        stateCapitalPairs.put("\nNew Mexico", "\nSanta Fe");
-        stateCapitalPairs.put("\nNew York", "\nAlbany");
-        stateCapitalPairs.put("\nNorth Carolina", "\nRaleigh");
-        stateCapitalPairs.put("\nNorth Dakota", "\nBismarck");
-        stateCapitalPairs.put("\nOhio", "\nColumbus");
-        stateCapitalPairs.put("\nOklahoma", "\nOklahoma City");
-        stateCapitalPairs.put("\nOregon", "\nSalem");
-        stateCapitalPairs.put("\nPennsylvania", "\nHarrisburg");
-        stateCapitalPairs.put("\nRhode Island", "\nProvidence");
-        stateCapitalPairs.put("\nSouth Carolina", "\nColumbia");
-        stateCapitalPairs.put("\nSouth Dakota", "\nPierre");
-        stateCapitalPairs.put("\nTennessee", "\nNashville");
-        stateCapitalPairs.put("\nTexas", "\nAustin");
-        stateCapitalPairs.put("\nUtah", "\nSalt Lake City");
-        stateCapitalPairs.put("\nVermont", "\nMontpelier");
-        stateCapitalPairs.put("\nVirginia", "\nRichmond");
-        stateCapitalPairs.put("\nWashington", "\nOlympia");
-        stateCapitalPairs.put("\nWest Virginia", "\nCharleston");
-        stateCapitalPairs.put("\nWisconsin", "\nMadison");
-        stateCapitalPairs.put("\nWyoming", "\nCheyenne");
+        stateCapitalPairs.put("Alabama", "Montgomery");
+        stateCapitalPairs.put("Alaska", "Juneau");
+        stateCapitalPairs.put("Arizona", "Phoenix");
+        stateCapitalPairs.put("Arkansas", "Little Rock");
+        stateCapitalPairs.put("California", "Sacramento");
+        stateCapitalPairs.put("Colorado", "Denver");
+        stateCapitalPairs.put("Connecticut", "Hartford");
+        stateCapitalPairs.put("Delaware", "Dover");
+        stateCapitalPairs.put("Florida", "Tallahassee");
+        stateCapitalPairs.put("Georgia", "Atlanta");
+        stateCapitalPairs.put("Hawaii", "Honolulu");
+        stateCapitalPairs.put("Idaho", "Boise");
+        stateCapitalPairs.put("Illinois", "Springfield");
+        stateCapitalPairs.put("Indiana", "Indianapolis");
+        stateCapitalPairs.put("Iowa", "Des Moines");
+        stateCapitalPairs.put("Kansas", "Topeka");
+        stateCapitalPairs.put("Kentucky", "Frankfort");
+        stateCapitalPairs.put("Louisiana", "Baton Rouge");
+        stateCapitalPairs.put("Maine", "Augusta");
+        stateCapitalPairs.put("Maryland", "Annapolis");
+        stateCapitalPairs.put("Massachusetts", "Boston");
+        stateCapitalPairs.put("Michigan", "Lansing");
+        stateCapitalPairs.put("Minnesota", "St. Paul");
+        stateCapitalPairs.put("Mississippi", "Jackson");
+        stateCapitalPairs.put("Missouri", "Jefferson City");
+        stateCapitalPairs.put("Montana", "Helena");
+        stateCapitalPairs.put("Nebraska", "Lincoln");
+        stateCapitalPairs.put("Nevada", "Carson City");
+        stateCapitalPairs.put("New Hampshire", "Concord");
+        stateCapitalPairs.put("New Jersey", "Trenton");
+        stateCapitalPairs.put("New Mexico", "Santa Fe");
+        stateCapitalPairs.put("New York", "Albany");
+        stateCapitalPairs.put("North Carolina", "Raleigh");
+        stateCapitalPairs.put("North Dakota", "Bismarck");
+        stateCapitalPairs.put("Ohio", "Columbus");
+        stateCapitalPairs.put("Oklahoma", "Oklahoma City");
+        stateCapitalPairs.put("Oregon", "Salem");
+        stateCapitalPairs.put("Pennsylvania", "Harrisburg");
+        stateCapitalPairs.put("Rhode Island", "Providence");
+        stateCapitalPairs.put("South Carolina", "Columbia");
+        stateCapitalPairs.put("South Dakota", "Pierre");
+        stateCapitalPairs.put("Tennessee", "Nashville");
+        stateCapitalPairs.put("Texas", "Austin");
+        stateCapitalPairs.put("Utah", "Salt Lake City");
+        stateCapitalPairs.put("Vermont", "Montpelier");
+        stateCapitalPairs.put("Virginia", "Richmond");
+        stateCapitalPairs.put("Washington", "Olympia");
+        stateCapitalPairs.put("West Virginia", "Charleston");
+        stateCapitalPairs.put("Wisconsin", "Madison");
+        stateCapitalPairs.put("Wyoming", "Cheyenne");
+    }
+
+    public static void infoPrinter(Collection<String> collection) {
+        for (String positionValue : collection) {
+            System.out.println(positionValue);
+        }
+    }
+
+    public static void infoPrinter(Collection<String> collection, Map<String, String> map) {
+
+        for (String positionValue : collection) {
+            System.out.println(positionValue + " - " + map.get(positionValue));
+        }
     }
 }
