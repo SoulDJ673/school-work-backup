0b902f64e3233099e4baf27723142905e8abb6ce
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-09-07 10:54:38 -0400

    We ended up finishing the File Reading/Writing code along

--- Milestone2/src/main/java/com/tsguild/milestone2/codealong/files/FileReading.java
+++ Milestone2/src/main/java/com/tsguild/milestone2/codealong/files/FileReading.java
@@ -1,9 +1,11 @@
 package com.tsguild.milestone2.codealong.files;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -15,14 +17,38 @@ public class FileReading {
         //Individual Llama Line
         String llamaAsString = "Jeff::Blue::Spiky::7::7.0::true";
         Llama jeff = unmarshallLlama(llamaAsString);
-        System.out.println("Jeff is back lol.");
+        System.out.println("Jeff is back lol.\n");
 
         //Getting the llamaHerd back together
         List<Llama> llamaHerd = new ArrayList<>();
 
         //Mass resurrection
+        //Finding the file
         String fileName = "llamaHerd.txt";
         FileReader fileReader = new FileReader(fileName);
+
+        //Reading the file
+        BufferedReader reader = new BufferedReader(fileReader);
+
+        //File Parsing
+        Scanner scanner = new Scanner(reader);
+
+        //File Iteration
+        while (scanner.hasNextLine()) {
+            String llamaLine = scanner.nextLine();
+            Llama theLlama = unmarshallLlama(llamaLine);
+            llamaHerd.add(theLlama);
+        }
+
+        //Kill scanner
+        scanner.close();
+
+        //Print Herd size and llama names
+        System.out.println("I read the file! I found: " + llamaHerd.size() + " llamas in it!");
+        System.out.println("Their names are: ");
+        for (Llama llama : llamaHerd) {
+            System.out.print(llama.getName() + ", ");
+        }
     }
 
 //Recreate Llamas from our marshalled LlamaHerd (Unmarshalling)
