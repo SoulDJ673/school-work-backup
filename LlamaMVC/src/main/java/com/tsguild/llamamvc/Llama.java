2bdd2ba867a9b46f607b661e3301145bb055d346
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-09-21 10:14:52 -0400

    Llama MVC Data Marshalling/Unmarshalling

--- LlamaMVC/src/main/java/com/tsguild/llamamvc/Llama.java
+++ LlamaMVC/src/main/java/com/tsguild/llamamvc/Llama.java
@@ -8,8 +8,167 @@ public class Llama {
 
     //Properties
     private int id;
-    
+
+    private String name;
+    private String color;
+    private String woolType;
+    private String favHangoutSpot;
+
+    private int age;
+    private int weight;
+    private int spitRange;
+    private int overBP;
+    private int underBP;
+
+    private long numHairs;
+    private boolean likesToKickJames;
+
+    private double tongueLength;
+    private double gpa;
+    private double omegaSpit;
+
+    //No Args
+    public Llama() {
+    }
+
+    //Just ID
+    public Llama(int id) {
+        this.id = id;
+    }
+
+    //All Args
+    public Llama(int id, String name, String color, String woolType, String favHangoutSpot, int age, int weight, int spitRange,
+            int overBP, int underBP, long numHairs, boolean likesToKickJames, double tongueLength, double gpa, double omegaSpit) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.woolType = woolType;
+        this.favHangoutSpot = favHangoutSpot;
+        this.age = age;
+        this.weight = weight;
+        this.spitRange = spitRange;
+        this.overBP = overBP;
+        this.underBP = underBP;
+        this.numHairs = numHairs;
+        this.likesToKickJames = likesToKickJames;
+        this.tongueLength = tongueLength;
+        this.gpa = gpa;
+        this.omegaSpit = omegaSpit;
+    }
+
     //Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getWoolType() {
+        return woolType;
+    }
+
+    public void setWoolType(String woolType) {
+        this.woolType = woolType;
+    }
+
+    public String getFavHangoutSpot() {
+        return favHangoutSpot;
+    }
+
+    public void setFavHangoutSpot(String favHangoutSpot) {
+        this.favHangoutSpot = favHangoutSpot;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getSpitRange() {
+        return spitRange;
+    }
+
+    public void setSpitRange(int spitRange) {
+        this.spitRange = spitRange;
+    }
+
+    public int getOverBP() {
+        return overBP;
+    }
+
+    public void setOverBP(int overBP) {
+        this.overBP = overBP;
+    }
+
+    public int getUnderBP() {
+        return underBP;
+    }
+
+    public void setUnderBP(int underBP) {
+        this.underBP = underBP;
+    }
+
+    public long getNumHairs() {
+        return numHairs;
+    }
+
+    public void setNumHairs(long numHairs) {
+        this.numHairs = numHairs;
+    }
+
+    public boolean isLikesToKickJames() {
+        return likesToKickJames;
+    }
+
+    public void setLikesToKickJames(boolean likesToKickJames) {
+        this.likesToKickJames = likesToKickJames;
+    }
+
+    public double getTongueLength() {
+        return tongueLength;
+    }
+
+    public void setTongueLength(double tongueLength) {
+        this.tongueLength = tongueLength;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public double getOmegaSpit() {
+        return omegaSpit;
+    }
+
+    public void setOmegaSpit(double omegaSpit) {
+        this.omegaSpit = omegaSpit;
+    }
+
     public int getId() {
         return this.id;
     }
--- LlamaMVC/src/main/java/com/tsguild/llamamvc/LlamaDaoImpl.java
+++ LlamaMVC/src/main/java/com/tsguild/llamamvc/LlamaDaoImpl.java
@@ -1,9 +1,16 @@
 package com.tsguild.llamamvc;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 /**
@@ -13,6 +20,119 @@ import java.util.TreeMap;
 public class LlamaDaoImpl implements LlamaDao {
 
     private Map<Integer, Llama> llamaHerd = new TreeMap<>();
+    private final String DELIMITER = "::";
+
+    /**
+     * This method takes a Llama and turns it into a single line text
+     * representation to allow being saved into a file, database, etc.
+     *
+     * The text is organized as follows:
+     * llamaID::llamaName::llamaAge::llamaWeight::llamaColor::llamaWoolType::
+     * llamaNumHair::llamaOverBP::llamaUnderBP::llamaSpitRange::llamaOmegaSpit::
+     * llamaTongueLength::llamaKick::llamaGPA::llamaHangoutSpot
+     *
+     * @param llamaToTextify - Llama to be represented by the line of text
+     */
+    public String marshallLlama(Llama llamaToTextify) {
+        String llamaText = "";
+
+        //Stringify
+        llamaText = llamaText + llamaToTextify.getId();
+        llamaText += DELIMITER + llamaToTextify.getName();
+        llamaText += DELIMITER + llamaToTextify.getAge();
+        llamaText += DELIMITER + llamaToTextify.getWeight();
+        llamaText += DELIMITER + llamaToTextify.getColor();
+        llamaText += DELIMITER + llamaToTextify.getWoolType();
+        llamaText += DELIMITER + llamaToTextify.getNumHairs();
+        llamaText += DELIMITER + llamaToTextify.getOverBP();
+        llamaText += DELIMITER + llamaToTextify.getUnderBP();
+        llamaText += DELIMITER + llamaToTextify.getSpitRange();
+        llamaText += DELIMITER + llamaToTextify.getOmegaSpit();
+        llamaText += DELIMITER + llamaToTextify.getTongueLength();
+        llamaText += DELIMITER + llamaToTextify.isLikesToKickJames();
+        llamaText += DELIMITER + llamaToTextify.getGpa();
+        llamaText += DELIMITER + llamaToTextify.getFavHangoutSpot();
+
+        return llamaText;
+    }
+
+    /**
+     * This is the inverse of marshallLlama. A line of text is turned back into
+     * a Llama.
+     *
+     * @param aLlamaLine - The text version of a llama, found in LlamaHerd.txt
+     * @return The Llama that was represented by the line of text
+     */
+    public Llama unmarshallLlama(String aLlamaLine) {
+        //split the line
+        String[] llamaBits = aLlamaLine.split(DELIMITER);
+
+        //Blank Llama Creation
+        Llama blankLlama = new Llama();
+
+        //Indexing
+        blankLlama.setId(Integer.parseInt(llamaBits[0]));
+        blankLlama.setName(llamaBits[1]);
+        blankLlama.setAge(Integer.parseInt(llamaBits[2]));
+        blankLlama.setWeight(Integer.parseInt(llamaBits[3]));
+        blankLlama.setColor(llamaBits[4]);
+        blankLlama.setWoolType(llamaBits[5]);
+        blankLlama.setNumHairs(Long.parseLong(llamaBits[6]));
+        blankLlama.setOverBP(Integer.parseInt(llamaBits[7]));
+        blankLlama.setUnderBP(Integer.parseInt(llamaBits[8]));
+        blankLlama.setSpitRange(Integer.parseInt(llamaBits[8]));
+        blankLlama.setOmegaSpit(Double.parseDouble(llamaBits[9]));
+        blankLlama.setTongueLength(Double.parseDouble(llamaBits[10]));
+        blankLlama.setLikesToKickJames(Boolean.parseBoolean(llamaBits[11]));
+        blankLlama.setGpa(Double.parseDouble(llamaBits[12]));
+        blankLlama.setFavHangoutSpot(llamaBits[13]);
+
+        return blankLlama;
+
+    }
+
+    /**
+     * Iterates through a generated list used to house the llamas in memory,
+     * marshall all llamas with marshallLlama method and write them to
+     * LlamaHerd.txt
+     *
+     * @throws java.io.IOException
+     */
+    public void saveAllLlamas() throws IOException {
+        //Setup the file
+        PrintWriter writer = new PrintWriter(new FileWriter("LlamaHerd.txt"));
+
+        //Assemble
+        List<Llama> allLlamas = this.getAllLlamas();
+
+        //Iteration
+        for (Llama aLlama : allLlamas) {
+            //Marshall Llama
+            String llamaText = this.marshallLlama(aLlama);
+            //Save to file
+            writer.println(llamaText);
+        }
+        //Polite
+        writer.flush();
+        writer.close();
+    }
+
+    public void readAllLlamas() throws FileNotFoundException {
+        //Scanner Opening
+        Scanner fileParser = new Scanner(new BufferedReader(
+                new FileReader("LlamaHerd.txt")));
+
+        //File Reader Setup
+        String fileName = "LlamaHerd.txt";
+
+        //File Breakdown
+        while (fileParser.hasNextLine()) {
+            String fileLine = fileParser.nextLine(); //Get Line
+            Llama fileLlama = this.unmarshallLlama(fileLine); // Line to Llama
+            this.llamaHerd.put(fileLlama.getId(), fileLlama); // Store in herd
+        }
+
+    }
 
     @Override
     public Llama addLlama(Llama aLlama) {
