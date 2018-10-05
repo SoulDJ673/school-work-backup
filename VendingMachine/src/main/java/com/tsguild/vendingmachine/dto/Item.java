96d8eb2be4899e2dc0b78477c3f5454517ba4acf
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-10-05 10:21:52 -0400

    VendingMachine - Finished Item for now, continuing unmarshall

--- VendingMachine/src/main/java/com/tsguild/vendingmachine/dto/Item.java
+++ VendingMachine/src/main/java/com/tsguild/vendingmachine/dto/Item.java
@@ -6,13 +6,42 @@ package com.tsguild.vendingmachine.dto;
  */
 public class Item {
 
-    private String id;
+    private String slotId;
+    private String itemName;
+    private double itemCost;
+    private int itemCount;
 
+    //Constructor
     public Item(String id) {
-        this.id = id;
+        this.slotId = id;
+    }
+
+    //Getters/Setters
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItemCost() {
+        return itemCost;
+    }
+
+    public void setItemCost(double itemCost) {
+        this.itemCost = itemCost;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
     }
 
     public String getId() {
-        return id;
+        return slotId;
     }
 }
