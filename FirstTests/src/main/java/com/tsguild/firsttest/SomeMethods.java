f4f3f6f3d507409d4216db3d7703d211df1d2174
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-10-29 08:53:05 -0400

    First Tests Assignment added to repo

---
+++ FirstTests/src/main/java/com/tsguild/firsttest/SomeMethods.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsguild.firsttest;
+
+/**
+ *
+ * @author ahill
+ */
+public class SomeMethods {
+    
+    public static int addStuff(int a, int b){
+        return a + a;
+    }
+
+    public static int multiplyStuff(int a, int b){
+        return a * a * b;
+    }
+
+    public static int returnTheBigger(int a, int b){
+        return a < b ? a : b;
+    }
+    
+}
