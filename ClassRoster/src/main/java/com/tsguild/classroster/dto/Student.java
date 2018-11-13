ae230ea08287a681652c373859a656220e65ed59
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-11-13 09:30:01 -0500

    Added equals() and hashCode() to Student DTO // toString() Override in Student

--- ClassRoster/src/main/java/com/tsguild/classroster/dto/Student.java
+++ ClassRoster/src/main/java/com/tsguild/classroster/dto/Student.java
@@ -1,5 +1,7 @@
 package com.tsguild.classroster.dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author souldj673
@@ -11,10 +13,12 @@ public class Student {
     private String studentId;
     private String cohort; // Java cohort month/year
 
+    //Constructor
     public Student(String studentId) {
         this.studentId = studentId;
     }
 
+    //Getters / Setters
     public String getFirstName() {
         return firstName;
     }
@@ -41,5 +45,49 @@ public class Student {
 
     public void setCohort(String cohort) {
         this.cohort = cohort;
+    }
+
+    //equals & hashCode
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.firstName);
+        hash = 59 * hash + Objects.hashCode(this.lastName);
+        hash = 59 * hash + Objects.hashCode(this.studentId);
+        hash = 59 * hash + Objects.hashCode(this.cohort);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.studentId, other.studentId)) {
+            return false;
+        }
+        if (!Objects.equals(this.cohort, other.cohort)) {
+            return false;
+        }
+        return true;
+    }
+
+    //toString() Fix
+    @Override
+    public String toString() {
+        return "Student{" + "firstName=" + firstName + ", lastName=" + lastName + ", studentId=" + studentId + ", cohort=" + cohort + '}';
     }
 }
