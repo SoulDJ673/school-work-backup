48b796e41b9abe5c152b10ade560bc6303243576
Devin Lloyd <devin.lloyd@stu.shelby.kyschools.us>
2018-11-13 09:21:59 -0500

    Added MonsterDAO Testing to Repo // Working on Student Roster Stateful Testings

--- ClassRoster/src/main/java/com/tsguild/classroster/dao/ClassRosterDaoFileImpl.java
+++ ClassRoster/src/main/java/com/tsguild/classroster/dao/ClassRosterDaoFileImpl.java
@@ -20,9 +20,19 @@ import java.util.Scanner;
 public class ClassRosterDaoFileImpl implements ClassRosterDao {
 
     //File Persistence
-    public static final String ROSTER_FILE = "roster.txt";
     public static final String DELIMITER = "::";
+    public final String ROSTER_FILE;
 
+    //Constructors
+    public ClassRosterDaoFileImpl() {
+        ROSTER_FILE = "roster.txt";
+    }
+
+    public ClassRosterDaoFileImpl(String rosterTextFile) {
+        ROSTER_FILE = rosterTextFile;
+    }
+
+    //Behaviors
     @Override
     public Student addStudent(String studentId, Student student) throws ClassRosterPersistenceException {
         loadRoster();
@@ -125,68 +135,68 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
         scanner.close();
     }
 
-    	private String marshallStudent(Student aStudent){
-	    // We need to turn a Student object into a line of text for our file.
-	    // For example, we need an in memory object to end up like this:
-	    // 4321::Charles::Babbage::Java-September1842
+    private String marshallStudent(Student aStudent) {
+        // We need to turn a Student object into a line of text for our file.
+        // For example, we need an in memory object to end up like this:
+        // 4321::Charles::Babbage::Java-September1842
 
-	    // It's not a complicated process. Just get out each property,
-	    // and concatenate with our DELIMITER as a kind of spacer. 
-	    
-	    // Start with the student id, since that's supposed to be first.
-	    String studentAsText = aStudent.getStudentId() + DELIMITER;
-	    
-	    // add the rest of the properties in the correct order:
+        // It's not a complicated process. Just get out each property,
+        // and concatenate with our DELIMITER as a kind of spacer. 
+        // Start with the student id, since that's supposed to be first.
+        String studentAsText = aStudent.getStudentId() + DELIMITER;
 
-	    // FirstName
-	    studentAsText += aStudent.getFirstName() + DELIMITER;
-		
-	    // LastName
-	    studentAsText += aStudent.getLastName() + DELIMITER;
-				
-	    // Cohort - don't forget to skip the DELIMITER here.
-	    studentAsText += aStudent.getCohort();
-		
-	    // We have now turned a student to text! Return it!
-	    return studentAsText;
-	}
-	/**
-	 * Writes all students in the roster out to a ROSTER_FILE.  See loadRoster
-	 * for file format.
-	 * 
-	 * @throws ClassRosterPersistenceException if an error occurs writing to the file
-	 */
-	private void writeRoster() throws ClassRosterPersistenceException {
-	    // NOTE FOR APPRENTICES: We are not handling the IOException - but
-	    // we are translating it to an application specific exception and 
-	    // then simple throwing it (i.e. 'reporting' it) to the code that
-	    // called us.  It is the responsibility of the calling code to 
-	    // handle any errors that occur.
-	    PrintWriter out;
-	    
-	    try {
-	        out = new PrintWriter(new FileWriter(ROSTER_FILE));
-	    } catch (IOException e) {
-	        throw new ClassRosterPersistenceException(
-	                "Could not save student data.", e);
-	    }
-	    
-	    // Write out the Student objects to the roster file.
-	    // NOTE TO THE APPRENTICES: We could just grab the student map,
-	    // get the Collection of Students and iterate over them but we've
-	    // already created a method that gets a List of Students so
-	    // we'll reuse it.
-	    String studentAsText;
-	    List<Student> studentList = this.getAllStudents();
-	    for (Student currentStudent : studentList) {
-	    	// turn a Student into a String
-	    	studentAsText = marshallStudent(currentStudent);
-	        // write the Student object to the file
-	        out.println(studentAsText);
-	        // force PrintWriter to write line to the file
-	        out.flush();
-	    }
-	    // Clean up
-	    out.close();
-	}
+        // add the rest of the properties in the correct order:
+        // FirstName
+        studentAsText += aStudent.getFirstName() + DELIMITER;
+
+        // LastName
+        studentAsText += aStudent.getLastName() + DELIMITER;
+
+        // Cohort - don't forget to skip the DELIMITER here.
+        studentAsText += aStudent.getCohort();
+
+        // We have now turned a student to text! Return it!
+        return studentAsText;
+    }
+
+    /**
+     * Writes all students in the roster out to a ROSTER_FILE. See loadRoster
+     * for file format.
+     *
+     * @throws ClassRosterPersistenceException if an error occurs writing to the
+     * file
+     */
+    private void writeRoster() throws ClassRosterPersistenceException {
+        // NOTE FOR APPRENTICES: We are not handling the IOException - but
+        // we are translating it to an application specific exception and 
+        // then simple throwing it (i.e. 'reporting' it) to the code that
+        // called us.  It is the responsibility of the calling code to 
+        // handle any errors that occur.
+        PrintWriter out;
+
+        try {
+            out = new PrintWriter(new FileWriter(ROSTER_FILE));
+        } catch (IOException e) {
+            throw new ClassRosterPersistenceException(
+                    "Could not save student data.", e);
+        }
+
+        // Write out the Student objects to the roster file.
+        // NOTE TO THE APPRENTICES: We could just grab the student map,
+        // get the Collection of Students and iterate over them but we've
+        // already created a method that gets a List of Students so
+        // we'll reuse it.
+        String studentAsText;
+        List<Student> studentList = this.getAllStudents();
+        for (Student currentStudent : studentList) {
+            // turn a Student into a String
+            studentAsText = marshallStudent(currentStudent);
+            // write the Student object to the file
+            out.println(studentAsText);
+            // force PrintWriter to write line to the file
+            out.flush();
+        }
+        // Clean up
+        out.close();
+    }
 }
