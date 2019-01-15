0a0674cb35c29ba5592c02ed7caf6b44efac0797
Devin Lloyd <souldj673@protonmail.com>
2019-01-15 10:32:02 -0500

    Mostly done with Hover events, Can't get header table row to not change color

--- WebFront/jQuery/jQuery-Browser-Events/js/home.js
+++ WebFront/jQuery/jQuery-Browser-Events/js/home.js
@@ -1,3 +1,57 @@
 $(document).ready(function () {
-    
+    /* Hide All Except Main on Load */
+    $('#akronInfoDiv').hide();
+    $('#minneapolisInfoDiv').hide();
+    $('#louisvilleInfoDiv').hide();
+
+    /* Hide All Weather by Default */
+    $('#akronWeather').hide();
+    $('#minneapolisWeather').hide();
+    $('#louisvilleWeather').hide();
+
+    /* Nav Buttons */
+    $('#mainButton').on("click", function () {
+        $('#mainInfoDiv').show();
+        $('#akronInfoDiv').hide();
+        $('#minneapolisInfoDiv').hide();
+        $('#louisvilleInfoDiv').hide();
+    });
+    $('#akronButton').on("click", function () {
+        $('#mainInfoDiv').hide();
+        $('#akronInfoDiv').show();
+        $('#minneapolisInfoDiv').hide();
+        $('#louisvilleInfoDiv').hide();
+    });
+    $('#minneapolisButton').on("click", function () {
+        $('#mainInfoDiv').hide();
+        $('#akronInfoDiv').hide();
+        $('#minneapolisInfoDiv').show();
+        $('#louisvilleInfoDiv').hide();
+    });
+    $('#louisvilleButton').on("click", function () {
+        $('#mainInfoDiv').hide();
+        $('#akronInfoDiv').hide();
+        $('#minneapolisInfoDiv').hide();
+        $('#louisvilleInfoDiv').show();
+    });
+
+    /* Weather Buttons */
+    $('#akronWeatherButton').on("click", function () {
+        $('#akronWeather').toggle("slow");
+    });
+    $('#minneapolisWeatherButton').on("click", function () {
+        $('#minneapolisWeather').toggle("slow");
+    });
+    $('#louisvilleWeatherButton').on("click", function () {
+        $('#louisvilleWeather').toggle("slow");
+    });
+
+    /* Table Rows */
+    $('tr').hover(function () {
+        /* In Callback */
+        $(this).css({ "background-color": "WhiteSmoke" });
+    }, function () {
+        /* Out Callback */
+        $(this).css({ "background-color": "" });
+    });
 });
