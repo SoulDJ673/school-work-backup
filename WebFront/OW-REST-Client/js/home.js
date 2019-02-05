c6af01e43bf39bd323557c3f8970e32a6573988b
Devin Lloyd <souldj673@protonmail.com>
2019-02-05 11:53:20 -0500

    Fixed jQuery issue

--- WebFront/OW-REST-Client/js/home.js
+++ WebFront/OW-REST-Client/js/home.js
@@ -1,2 +1,17 @@
 $(document).ready(function () {
+
+});
+
+getWeatherData(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&appid=e52123ca9924186bd5b93d59366b7e55',
+        success: function () {
+            /* Turn JSON into Displayable content */
+        },
+        error: function () {
+            $('#errorMessages').append($('<li>').attr({ class: 'list-group-item list-group-item-danger' })
+                .text('Error calling web service.  Try again later.'));
+        }
+    });
 });
