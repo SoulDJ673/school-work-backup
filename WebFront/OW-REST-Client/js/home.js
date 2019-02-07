2287788601ca52ba1c188a987dbb0d61bb2340fa
Devin Lloyd <souldj673@protonmail.com>
2019-02-07 09:06:18 -0500

    Info would submit before button click (fixed).

--- WebFront/OW-REST-Client/js/home.js
+++ WebFront/OW-REST-Client/js/home.js
@@ -1,17 +1,41 @@
 $(document).ready(function () {
+    /* Toggle Input Fields */
+    $('#hideInputFields').on("click", function (event) {
+        $('#hideInputFields').hide();
+        $('#getInfo').hide();
+        $('#showInputFields').show();
+    });
 
-});
+    $('#showInputFields').on("click", function (event) {
+        $('#showInputFields').hide();
+        $('#hideInputFields').show();
+        $('#getInfo').show();
+    });
 
-getWeatherData(function () {
-    $.ajax({
-        type: 'GET',
-        url: 'api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&appid=e52123ca9924186bd5b93d59366b7e55',
-        success: function () {
-            /* Turn JSON into Displayable content */
-        },
-        error: function () {
-            $('#errorMessages').append($('<li>').attr({ class: 'list-group-item list-group-item-danger' })
-                .text('Error calling web service.  Try again later.'));
-        }
+    $('#infoSubmitButton').on("click", function (event) {
+        getWeatherData();
     });
 });
+
+function getWeatherData() {
+    var zipCode = $('#enterZipCode').val();
+    var units = $('#unitDropdown').val();
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&appid=e52123ca9924186bd5b93d59366b7e55',
+        success: function (data) {
+            /* Turn JSON into Displayable Data */
+            var city = data.name;
+            var condition = data.weather.main;
+            var description = data.weather.description;
+
+            console.log(data);
+        },
+        error: function () {
+            $('#headerDiv').hide();
+            $('#errorCage').show();
+            $('#errorCage').prepend($('<p>').text('Error calling web service.  Try again later.'));
+        }
+    });
+};
