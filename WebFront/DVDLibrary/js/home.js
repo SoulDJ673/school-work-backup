e0a4772091d45738caf769e20b904c5792400a68
Devin Lloyd <souldj673@protonmail.com>
2019-02-19 10:58:43 -0500

    (DVD Library) Finished delete confirmation, starting ajax calls

--- WebFront/DVDLibrary/js/home.js
+++ WebFront/DVDLibrary/js/home.js
@@ -1,3 +1,69 @@
 $(document).ready(function () {
-
 });
+
+function populateLibrary() {
+    clearDVDLibraryTable();
+    var contentRows = $('#dvdSection');
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/contacts',
+        success: function (contactArray) {
+            $.each(contactArray, function (index, contact) {
+                var name = contact.firstName + ' ' + contact.lastName;
+                var company = contact.company;
+                var contactId = contact.contactId;
+
+                var row = '<tr>';
+                row += '<td>' + name + '</td>';
+                row += '<td>' + company + '</td>';
+                row += '<td><a onclick="showEditForm(' + contactId + ')">Edit</a></td>';
+                row += '<td><a onclick="deleteContact(' + contactId + ')">Delete</a></td>';
+                row += '</tr>';
+
+                contentRows.append(row);
+            });
+        },
+        error: function () {
+            $('#errorMessages').append($('<li>').attr({ class: 'list-group-item list-group-item-danger' })
+                .text('Error calling web service.  Try again later.'));
+        }
+    });
+}
+
+function clearDVDLibraryTable() {
+    $('#dvdSection').empty();
+}
+
+function showDeleteConfirmDiv() {
+    $('#deleteConfirmDiv').show();
+
+    /* Make Action Bar Unusable until user input */
+    $('#actionBarDiv').hide();
+    return;
+}
+
+function hideDeleteConfirmDiv() {
+    $('#deleteConfirmDiv').fadeOut();
+
+    /* Re-enable Action Bar */
+    $('#actionBarDiv').fadeIn();
+    return;
+}
+
+function deleteDVD(dvdId) {
+    var id = dvdId;
+    showDeleteConfirmDiv();
+    $('#confirmCancel').click(function (event) {
+        hideDeleteConfirmDiv();
+        return;
+    });
+
+    $('#confirmDeleteButton').click(function (event) {
+        console.log(id);
+        hideDeleteConfirmDiv();
+        return;
+    });
+
+    return;
+}
