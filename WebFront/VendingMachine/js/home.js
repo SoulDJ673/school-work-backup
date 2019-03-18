3d5b570e9f1bda2e15e6854f20b3d06be5805ce1
Devin Lloyd <souldj673@protonmail.com>
2019-03-18 11:56:18 -0400

    (Vending Machine) Loading Items JS into boxes complete

--- WebFront/VendingMachine/js/home.js
+++ WebFront/VendingMachine/js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    loadItems();
 });
 
 function addCoinage(value) {
@@ -22,7 +22,14 @@ function loadItems() {
                 var quantity = item.quantity;
 
                 //Add to table
-                var box = "<div onclick='$(" + '"#itemCodeInput"' + ").val(" + id + ")' class='flex-container flex-container-centered item-box'>"
+                var box = "<div onclick='$(" + '"#itemCodeInput"' + ").val(" + id + ")' class='item-box'>";
+                box += "<p style='top:10;left:10'>" + id + "</p>";
+                box += "<h4 class='text-center' style='margin:20px'>" + name + "</h4>"
+                box += "<h4 class='text-center' style='margin:20px'>$" + price + "</h4>";
+                box += "<h4 class='text-center' style='margin:20px'>Quantity Left: " + quantity + "</h4>";
+                box += "</div>";
+
+                $('#foodStuffs').append(box);
             });
         },
         error: function () {
