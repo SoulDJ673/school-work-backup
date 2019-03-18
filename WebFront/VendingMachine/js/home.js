aca4ccd186812f8f2bd88e8da2e3be515c94afcf
Devin Lloyd <souldj673@protonmail.com>
2019-03-18 10:55:17 -0400

    (Vending Machine) Working on loading item JS, boxes

--- WebFront/VendingMachine/js/home.js
+++ WebFront/VendingMachine/js/home.js
@@ -1,3 +1,32 @@
 $(document).ready(function () {
-    $('#depositedMoneyDisplay').val('$0.00');
+
 });
+
+function addCoinage(value) {
+
+};
+
+function purchaseItem(id) {
+
+};
+
+function loadItems() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/items',
+        success: function (data) {
+            $.each(data, function (index, item) {
+                var id = item.id;
+                var name = item.name;
+                var price = item.price;
+                var quantity = item.quantity;
+
+                //Add to table
+                var box = "<div onclick='$(" + '"#itemCodeInput"' + ").val(" + id + ")' class='flex-container flex-container-centered item-box'>"
+            });
+        },
+        error: function () {
+
+        }
+    });
+};
