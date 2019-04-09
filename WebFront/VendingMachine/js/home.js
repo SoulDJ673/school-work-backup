eb2d8f80f7f2293642367960c513d3adca109773
Devin Lloyd <souldj673@protonmail.com>
2019-04-09 09:32:26 -0400

    (Vending Machine) Added more comments

--- WebFront/VendingMachine/js/home.js
+++ WebFront/VendingMachine/js/home.js
@@ -15,7 +15,10 @@ function purchaseItem(id) {
         type: 'GET',
         url: 'http://localhost:8080/money/' + depositedMoney + '/item/' + id,
         success: function (status, data, response) {
+            //Remove any change still left in the change thingy
             var change = 0;
+
+            //Count up the change & display
             change += .10 * (response.responseJSON.dimes);
             change += .05 * (response.responseJSON.nickels);
             change += .01 * (response.responseJSON.pennies);
@@ -29,25 +32,29 @@ function purchaseItem(id) {
             loadItems();
         },
         error: function (headers) {
+            //API Automatically returns relevant error message, display that in message box
             $('#messagesOutput').val(headers.responseJSON.message);
         }
     });
 };
 
 function loadItems() {
+    //Start from scratch
     $('#foodStuffs').empty();
 
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/items',
         success: function (data) {
+            //Item selection factory PogU
             $.each(data, function (index, item) {
+                //Pre op
                 var id = item.id;
                 var name = item.name;
                 var price = item.price;
                 var quantity = item.quantity;
 
-                //Add to table
+                //Build selection box
                 var box = "<div onclick='$(" + '"#itemCodeInput"' + ").val(" + id + ")' class='item-box'>";
                 box += "<p style='top:10;left:10'>" + id + "</p>";
                 box += "<h4 class='text-center' style='margin:20px'>" + name + "</h4>"
@@ -55,6 +62,7 @@ function loadItems() {
                 box += "<h4 class='text-center' style='margin:20px'>Quantity Left: " + quantity + "</h4>";
                 box += "</div>";
 
+                //Add to grid
                 $('#foodStuffs').append(box);
             });
         },
