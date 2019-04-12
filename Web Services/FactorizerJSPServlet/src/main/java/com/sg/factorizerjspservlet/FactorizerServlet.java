0987d2d8bb921dd1057ffc4d198856ebc12656b1
Devin Lloyd <souldj673@protonmail.com>
2019-04-12 09:46:56 -0400

    (FactorizerJSP) Codealong finished.

--- Web Services/FactorizerJSPServlet/src/main/java/com/sg/factorizerjspservlet/FactorizerServlet.java
+++ Web Services/FactorizerJSPServlet/src/main/java/com/sg/factorizerjspservlet/FactorizerServlet.java
@@ -6,7 +6,9 @@
 package com.sg.factorizerjspservlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,21 +31,51 @@ public class FactorizerServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FactorizerServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet FactorizerServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        // A List to hold our factors
+        List<Integer> factorList = new ArrayList<>();
+        // A sum to help us calculate whether or not the number
+        // is perfect
+        int factorSum = 0;
+        boolean isPrime = false;
+        boolean isPerfect = false;
+        // Get the input from the user and convert it to an int
+        String input = request.getParameter("numberToFactor");
+        int numberToFactor = Integer.parseInt(input);
+
+        // Factor the number - there are more efficient ways
+        // of doing this!
+        for (int i = 1; i < numberToFactor; i++) {
+            if (numberToFactor % i == 0) {
+                // i goes into numberToFactor evenly so it
+                // is a factor, add it to the list and add
+                // it to the sum
+                factorList.add(i);
+                factorSum += i;
+            }
         }
+
+        if (factorSum == numberToFactor) {
+            isPerfect = true;
+        }
+
+        if (factorSum == 1) {
+            isPrime = true;
+        }
+
+        // Set all the results as attributes on the request to they
+        // are available to result.jsp
+        request.setAttribute("numberToFactor", numberToFactor);
+        request.setAttribute("factors", factorList);
+        request.setAttribute("isPrime", isPrime);
+        request.setAttribute("isPerfect", isPerfect);
+        // Get the Request Dispatcher for result.jsp and forward the 
+        // request to result.jsp
+        RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+        rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- Web Services/FactorizerJSPServlet/src/main/webapp/index.jsp
+++ Web Services/FactorizerJSPServlet/src/main/webapp/index.jsp
@@ -1,17 +1,18 @@
-<%-- 
-    Document   : index
-    Created on : Apr 11, 2019, 10:45:39 AM
-    Author     : souldj673
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Factorizor</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Factorizor</h1>
+        <p>
+            Please enter the number you want to factor:
+        </p>
+        <form method="post" action="FactorizerServlet">
+            <input type="text" name="numberToFactor"/><br/>
+            <input type="submit" value="Find Factors!!!"/>
+        </form>
     </body>
 </html>
